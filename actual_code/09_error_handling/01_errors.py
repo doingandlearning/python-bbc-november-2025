bc3e91dc4c2930bf6868a7f70d7b709ed94c4ccf
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-11-05 12:18:58 +0000

    Add code

--- actual_code/09_error_handling/01_errors.py
+++ actual_code/09_error_handling/01_errors.py
@@ -4,5 +4,27 @@
 - Logic error!
 
 2. Real-world Errors
+- user input
+- network connections
+- database
+
+Strategy - recovery ... 
+Users - a nice error message
+
+Error <> Exception
+
+FileNotFoundError
+
+try:
+  anything risky -> users, accessing filesystem, querying a db
+except:
+  handle the errors that we're expecting!
+
+try:
+  another risky
+except:
+
 
 """
+
+SyntaxError()
