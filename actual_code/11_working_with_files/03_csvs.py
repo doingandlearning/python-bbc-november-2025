8b5b6c87fbf06ed22ea3086e634b02db25bcd9fe
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-11-05 16:13:09 +0000

    Add code

--- actual_code/11_working_with_files/03_csvs.py
+++ actual_code/11_working_with_files/03_csvs.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 # with open("movies.csv") as file:
 #   reader = csv.reader(file)
@@ -12,6 +13,20 @@ import csv
 #   for row in reader:
 #     print(f"{row.get("title")} was made in {row.get("year")}, directed by {row.get("director")} in the genre {row.get("genre")}")
 
-with open("movies.csv", mode="a") as file:
-  writer = csv.writer(file)
-  writer.writerow(["K-Pop Demon Hunters", 2025, "Maggie Kang;Chris Appelhans", "Animation"])
+# with open("movies.csv", mode="a") as file:
+#   writer = csv.writer(file)
+#   writer.writerow(["K-Pop Demon Hunters", 2025, "Maggie Kang;Chris Appelhans", "Animation"])
+
+movies = []
+
+with open("movies.csv") as file:
+  reader = csv.DictReader(file)
+  for movie in reader:
+    movies.append(movie)
+
+musicals = [movie for movie in movies if movie["Genre"] == "Musical"]
+recent_movies = [movie for movie in movies if int(movie["Year"]) > 2020] 
+
+with open("movies.json", "w") as file:
+  json.dump(recent_movies, file, indent=2)
+
